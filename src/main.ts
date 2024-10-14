8755ac64e15281d7c9f7b747344e5cac3abfc71e
Elton Z <poptako9@gmail.com>
2024-10-14 15:09:54 -0700

    Main/changed the name of the app.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Hello My name is Jeff";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
