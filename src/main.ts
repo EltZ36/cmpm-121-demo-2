7e381c6594d19f2f5bd26c274b4a4bae938ec4b0
Elton Zeng <poptako9@gmail.com>
2024-10-15 21:44:41 -0700

    main/Finished up Step 1 and added in a canvas with a white background.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "Hello My name is Jeff";
+const APP_NAME = "Drawing App";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerText = "Draw down below";
+
+const drawingCanvas = document.createElement('canvas');
+drawingCanvas.id = "drawingCanvas";
+drawingCanvas.width = 256;
+drawingCanvas.height = 256;
+
+app.append(title);
+app.append(drawingCanvas);
