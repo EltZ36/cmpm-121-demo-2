c3aaa6132f5469872417c2a09e3f7320462ea053
Elton Zeng <poptako9@gmail.com>
2024-10-15 23:05:42 -0700

    main/Step 2 completed. Added a drawline function and the eventListeners for the mousedown, mouseup, and mousemove. I also adjusted the css to make the button go to the left of canvas, instead of to the side of it.

--- src/main.ts
+++ src/main.ts
@@ -16,3 +16,48 @@ drawingCanvas.height = 256;
 
 app.append(title);
 app.append(drawingCanvas);
+
+const ctx = drawingCanvas.getContext('2d')!;
+const cursor = { active: false, x : 0, y: 0};
+
+function drawLine(context: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number){
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+drawingCanvas.addEventListener("mousedown", (e) => {
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+    cursor.active = true;
+});
+
+drawingCanvas.addEventListener("mousemove", (e) => {
+    if(cursor.active){
+        drawLine(ctx, cursor.x, cursor.y, e.offsetX, e.offsetY);
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+drawingCanvas.addEventListener("mouseup", (e) => {
+    if(cursor.active){
+        drawLine(ctx, cursor.x, cursor.y, e.offsetX, e.offsetY)
+        cursor.x = 0;
+        cursor.y = 0;
+        cursor.active = false;
+    }
+});
+
+const clearButton = document.createElement('button');
+clearButton.id = "clearButton"
+clearButton.innerHTML = "clear";
+clearButton.addEventListener('click', () =>{
+    ctx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height)
+}); 
+
+app.append(clearButton);
